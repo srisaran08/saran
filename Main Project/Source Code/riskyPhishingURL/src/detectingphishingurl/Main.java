c88b00326c27990cc702a600b89901c04dbae32d
srisaran08 <saranelango0815@gmail.com>
2022-08-02 12:16:26 +0530

    final

---
+++ Main Project/Source Code/riskyPhishingURL/src/detectingphishingurl/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package detectingphishingurl;
+
+/**
+ *
+ * @author Elcot
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mf=new MainFrame();
+        mf.setVisible(true);
+        mf.setTitle("Main Frame");
+        mf.setResizable(false);
+    } 
+}
